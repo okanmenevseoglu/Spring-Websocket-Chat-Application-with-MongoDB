3c6c32b81f790848a195d2aa1a7476aa83b70709
Okan Menevseoglu <okan.menevseoglu@dolap.com>
2018-06-03 15:35:33 +0300

    Initial commit

---
+++ src/main/java/com/okanmenevseoglu/chatapp/ChatappApplication.java
@@ -0,0 +1,12 @@
+package com.okanmenevseoglu.chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatappApplication.class, args);
+    }
+}
